ebb538cecfa1c417be7e25178ce1598e1909ab41
pewpewnor <newpatchnotworking2@tutanota.com>
2023-05-29 10:09:06 +0700

    feat: remove unused components

--- src/page/workspace/thread/ThreadView.tsx
+++
@@ -1,9 +0,0 @@
-import { FC } from "react";
-
-interface ThreadViewProps {}
-
-const ThreadView: FC<ThreadViewProps> = (props: ThreadViewProps) => {
-	return <div>ThreadView</div>;
-};
-
-export default ThreadView;
